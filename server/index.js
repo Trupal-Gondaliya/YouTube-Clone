fd1e9bd2a29bd29fae3b5f03d6299c4b6835e158
Trupal-Gondaliya <trupalgondaliya@gmail.com>
2025-12-22 19:22:33 +0530

    setup express server with mongodb connection

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './db.js';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // To parse JSON bodies
+
+// Connect to Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
