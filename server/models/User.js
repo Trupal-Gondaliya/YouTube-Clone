97c01ace5d8df309374b3228bae0ddee46993972
Trupal-Gondaliya <trupalgondaliya@gmail.com>
2025-12-29 22:28:29 +0530

    edit schema

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true }, 
   email: { type: String, required: true, unique: true }, 
   password: { type: String, required: true }, 
-  avatar: { type: String, default: "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-High-Quality-Image.png"},
+  avatar: { type: String },
   channels: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Channel' }] 
 }, { timestamps: true });
 
--- server/models/Video.js
+++ server/models/Video.js
@@ -5,10 +5,10 @@ const videoSchema = new mongoose.Schema({
   thumbnailUrl: { type: String, required: true }, 
   videoUrl: { type: String, required: true },
   description: { type: String }, 
-  channelId: { type: mongoose.Schema.Types.ObjectId, ref: 'Channel' }, 
-  uploader: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+  channelId: { type: mongoose.Schema.Types.ObjectId, ref: 'Channel' , required: true }, 
+  uploader: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
   views: { type: Number, default: 0 }, 
-  likes: { type: Number, default: 0 }, 
+  likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 }, 
   category: { type: String, required: true }, 
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }] 
