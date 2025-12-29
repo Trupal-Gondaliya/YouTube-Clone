a49cabfe5aa33ec1d961341c9a3b3eec1bdd93a0
Trupal-Gondaliya <trupalgondaliya@gmail.com>
2025-12-29 22:29:08 +0530

    modify route path

--- server/routes/channel.js
+++ server/routes/channel.js
@@ -5,7 +5,7 @@ import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
 // Only signed-in users can create a channel
-router.post("/", verifyToken, createChannel);
+router.post("/create", verifyToken, createChannel);
 router.get("/:id", getChannel);
 
 export default router;
